6c29d490e5e112cf25ac668bc0bf6a25323f54db
Aidan Pine <hello@aidanpine.ca>
2025-06-25 14:47:52 -0700

    Update environment.prod.ts
    change middleware to sencoten.org

--- environment.prod.ts
+++ environment.prod.ts
@@ -12,7 +12,7 @@ export const environment = {
     requiresAuth: true,
     diffusionSteps: 3, // 👈  OPTIONAL: number of diffusion steps
     middlewareEndpoint:
-      "https://wsanec-tts-middleware-2372f187c744.herokuapp.com/tts",
+      "https://tts.sencoten.org/tts",
     domain: "wsanec-sgile.ca.auth0.com",
     clientId: "XTyHkE8Jnea0YCcQpdxPylluY49H6XKJ",
     audience: "https://wsanec.everyvoice.middleware.ca",
